08aee303ab6a4f8a37cb20fe911190755e54d33d
rangelrey <rogerangelrrp@gmail.com>
2019-11-02 08:53:36 +0100

    update

--- projects/blueJProjects/ListString.java
+++ projects/blueJProjects/ListString.java
@@ -1,62 +1,87 @@
 public class ListString {
-  private ListNode head;
+    private ListNode head;
 
-  // the constructor simply creates the class with a null value for "head"
-  public ListString () {
-    head = null;
-  }
-
-  // the join method creates a new ListNode for the given item of data
-  // and appends the new item to the end of the list
-  public void join (String data) {
-    if (head == null) {                         // <1>
-      head = new ListNode (null, data);
-      return;                                   // <2>
+    public ListString (String data) {
+        this.head = new ListNode(null,data);
     }
 
-    ListNode current = head;                    // <3>
+    public static void main() {
+        ListString miLista = new ListString("a");
 
-    // walk the list to find the last item
-    while (current.getNext () != null) {
-      current = current.getNext ();
+        miLista.join("b");
+        miLista.join("C");
+
+        System.out.println(miLista);
+
+        miLista.leave();
+        System.out.println(miLista);
+        miLista.get(1);
     }
 
-    // make a new node from data and place it in the lastItem
-    current.setNext (new ListNode (null, data));
-  }
+    // the constructor simply creates the class with a null value for "head"
+    public ListString () {
+        head = null;
+    }
 
-  public String leave () {
-    if (head != null) { 
-        
-        ListNode current = head;  
-        
-        while (current.getNext().getNext () == null) {
-            current = current.getNext ();
-            
+    // the join method creates a new ListNode for the given item of data
+    // and appends the new item to the end of the list
+    public void join (String data) {
+        if (head == null) {                         // <1>
+            head = new ListNode (null, data);
+            return;                                   // <2>
         }
-        current.setNext (null);
+
+        ListNode current = head;                    // <3>
+
+        // walk the list to find the last item
+        while (current.getNext () != null) {
+            current = current.getNext ();
+        }
+
+        // make a new node from data and place it in the lastItem
+        current.setNext (new ListNode (null, data));
     }
 
-      
-      
-    
-    return "";
-  }
+    public String leave () {
+        if (head == null) {
+            return null;
+        } else {
+            String result = head.getData ();
+            head = head.getNext ();
+            return result;
+        }
+    }
 
-  public String get (int index) {
-    // COMPLETE THIS METHOD
-    return "";
-  }
+    public String get (int index) {
+        if (head == null) {
+            throw new ArrayIndexOutOfBoundsException (index);
+        } else {
+            return head.get (index);
+        }
+    }
 
-  public void set (String data, int index) {
-    // COMPLETE THIS METHOD
-  }
+    public void set (String data, int index) {
+        if (head == null) {
+            throw new ArrayIndexOutOfBoundsException (index);
+        } else {
+            head.set(data,index);
+        }
+    }
 
-  public void insert (String data, int index) {   // <4>
-    // COMPLETE THIS METHOD
-  }
+    public void insert (String data, int index) {   // <4>
+        // COMPLETE THIS METHOD
+    }
 
-  public void delete (int index) {
-    // COMPLETE THIS METHOD
-  }
+    public void delete (int index) {
+        // COMPLETE THIS METHOD
+    }
+
+    public String toString () {
+        if (head == null) {
+            return "[]";
+        } else {
+            return head.toString ();
+        }
+    }
+
 }
